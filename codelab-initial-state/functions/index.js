62f2364048e4272228e0c740a05acac2197b3b82
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2021-05-19 11:35:22 +0800

    finished codelab tests passing

--- codelab-initial-state/functions/index.js
+++ codelab-initial-state/functions/index.js
@@ -12,31 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
 const db = admin.initializeApp().firestore();
 
 // Recalculates the total cost of a cart; triggered when there's a change
 // to any items in a cart.
-exports.calculateCart = functions
-    .firestore.document("carts/{cartId}/items/{itemId}")
-    .onWrite(async (change, context) => {
-      console.log(`onWrite: ${change.after.ref.path}`);
-      if (!change.after.exists) {
-        // Ignore deletes
-        return;
-      }
+exports.calculateCart = functions.firestore
+  .document("carts/{cartId}/items/{itemId}")
+  .onWrite(async (change, context) => {
+    console.log(`onWrite: ${change.after.ref.path}`);
+    if (!change.after.exists) {
+      // Ignore deletes
+      return;
+    }
 
-      let totalPrice = 125.98;
-      let itemCount = 8;
-      try {
+    let totalPrice = 0;
+    let itemCount = 0;
+    try {
+      const cartRef = db.collection("carts").doc(context.params.cartId);
 
-        const cartRef = db.collection("carts").doc(context.params.cartId);
-
-        await cartRef.update({
-          totalPrice,
-          itemCount
-        });
-      } catch(err) {
-      }
-    });
+      const itemsSnap = await cartRef.collection("items").get();
+      itemsSnap.docs.forEach((item) => {
+        const itemData = item.data();
+        if (itemData.price) {
+          const quantity = itemData.quantity ? itemData.quantity : 1;
+          itemCount += quantity;
+          let price = parseFloat(itemData.price);
+          totalPrice += price;
+        }
+      });
+      await cartRef.update({
+        totalPrice,
+        itemCount,
+      });
+      console.log("Cart successfully updated: ", totalPrice);
+    } catch (err) {
+      console.error("Error updating Cart", err);
+    }
+  });
